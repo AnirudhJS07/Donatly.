a161ec3e750874f764069adcadab19e98d6ad47c
Abhinav Aggrawal <abhinavagrawal101@gmail.com>
2021-10-24 00:08:31 +0530

    paypal integeration

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const ejs = require("ejs");
 const mongoose = require('mongoose');
 const https = require("https");
 const { url } = require("inspector");
+const paypal = require('paypal-rest-sdk');
 
 
 require('dotenv').config({ path: '.env' });
@@ -55,8 +56,14 @@ const contactSchema = {
   message: String
 };
 
+const donationSchema = {
+  amount :String,
+  details : Array
+}
+
 const User = mongoose.model("User", donorSchema );
 const Contact = mongoose.model("Contact" , contactSchema);
+const Donation = mongoose.model("Donation" , donationSchema);
 
 
 app.post("/form", function(req, res){
@@ -173,6 +180,98 @@ app.post("/newsletter",function(request,res){
   r.end();
 
 });
+
+
+
+// paypal 
+paypal.configure({
+  'mode': 'sandbox', //sandbox or live
+  'client_id': process.env.PAYPAL_CLIENT,
+  'client_secret': process.env.PAYPAL_SECRET
+});
+
+app.post('/donateus', (req, res) => {
+  const create_payment_json = {
+    "intent": "sale",
+    "payer": {
+        "payment_method": "paypal"
+    },
+    "redirect_urls": {
+        "return_url": "http://localhost:3000/success",
+        "cancel_url": "http://localhost:3000/cancel"
+    },
+    "transactions": [{
+        "item_list": {
+            "items": [{
+                "name": "Redhock Bar Soap",
+                "sku": "001",
+                "price": req.body.amount,
+                "currency": "USD",
+                "quantity": 1
+            }]
+        },
+        "amount": {
+            "currency": "USD",
+            "total": req.body.amount
+        },
+        "description": "Donations to donately"
+    }]
+};
+
+paypal.payment.create(create_payment_json, function (error, payment) {
+  if (error) {
+      throw error;
+  } else {
+      for(let i = 0;i < payment.links.length;i++){
+        if(payment.links[i].rel === 'approval_url'){
+          res.redirect(payment.links[i].href);
+        }
+      }
+  }
+});
+
+
+
+});
+
+
+app.get('/success', (req, res) => {
+  const payerId = req.query.PayerID;
+  const paymentId = req.query.paymentId;
+
+  const execute_payment_json = {
+    "payer_id": payerId,
+    "transactions": [{
+        "amount": {
+            "currency": "USD",
+            "total": "25.00"
+        }
+    }]
+  };
+
+// Obtains the transaction details from paypal
+  paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
+      //When error occurs when due to non-existent transaction, throw an error else log the transaction details in the console then send a Success string reposponse to the user.
+    if (error) {
+        console.log(error.response);
+        throw error;
+    } else {
+        // console.log(JSON.stringify(payment));
+        res.send('Success');
+        const newdonation = new Donation({
+          details: JSON.stringify(payment)
+        })
+
+    }
+});
+});
+
+app.get('/cancel', (req, res) => res.send('Cancelled'));
+
+
+
+
+
 
 
 app.get("/", function(req, res){
