24c712cd1b1c7054d76209a40d467c1ba56c3ad3
Abhinav Aggrawal <abhinavagrawal101@gmail.com>
2021-10-23 19:05:25 +0530

    env

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-require('dotenv').config();
+require('dotenv').config({ path: '.env' });
 
 // add your username and password in string below
 const mongo = process.env.MONGO_AUTH;
