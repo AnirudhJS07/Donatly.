ab99b7589115a9ef23a9536a58ac29e233c8e271
Abhinav Aggrawal <abhinavagrawal101@gmail.com>
2021-10-24 08:31:03 +0530

    paypal links

--- app.js
+++ app.js
@@ -197,8 +197,8 @@ app.post('/donateus', (req, res) => {
         "payment_method": "paypal"
     },
     "redirect_urls": {
-        "return_url": "http://localhost:3000/success",
-        "cancel_url": "http://localhost:3000/cancel"
+        "return_url": "http://donatly.herokuapp.com/success",
+        "cancel_url": "http://donatly.herokuapp.com/cancel"
     },
     "transactions": [{
         "item_list": {
